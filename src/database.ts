4a5487ddbbd824bc91796a147c0eebd29d0c9d04
João Victor <jv782063@gmail.com>
2020-12-16 18:50:47 -0300

    add dotenv

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,11 @@
-import config, { IConfig } from 'config';
 import mongoose from 'mongoose';
+import 'dotenv/config';
 
-const dbConfig: IConfig = config.get('App.database');
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const dbConfig: string = process.env.MONGO_URL as string;
 
 export const connect = async (): Promise<void> => {
-  await mongoose.connect(dbConfig.get('mongoUrl'), {
+  await mongoose.connect(dbConfig, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
